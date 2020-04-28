20228fe1749afd680071593e11574b9cc3953d68
sgigli <simon.luciano@gigli.net>
2020-04-28 18:11:01 -0400

    Init commit

---
+++ mongodb.js
@@ -0,0 +1,64 @@
+// CRUD create read update delete
+
+const mongodb = require('mongodb')
+const MongoClient = mongodb.MongoClient
+
+const connectionURL = 'mongodb://127.0.0.1:27017'
+const databaseName = 'task-manager'
+
+MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) => {
+    if (error) {
+        return console.log('Unable to connect to database!')
+    }
+
+    const db = client.db(databaseName)
+
+    // db.collection('users').insertOne({
+    //     name: 'Simon',
+    //     age: 25
+    // }, (error, result) => {
+    //     if (error) {
+    //         return console.log('Unable to insert user')
+    //     }
+
+    //     console.log(result.ops)
+    // })
+
+    // db.collection('users').insertMany([
+    //     {
+    //         name: 'Jen',
+    //         age: 28
+    //     },
+    //     {
+    //         name: 'Gunther',
+    //         age: 27
+    //     }
+    // ], (error, result) => {
+    //     if (error) {
+    //         return console.log('Unable to insert documents!')
+    //     }
+
+    //     console.log(result.ops)
+    // })
+
+    db.collection('tasks').insertMany([
+        {
+            description: 'dishes',
+            completed: true
+        }, {
+            description: 'homework',
+            completed: false
+        }, {
+            description: 'track practice',
+            completed: true
+        }
+
+    ], (error, result) => {
+        if (error) {
+            return console.log('Unable to insert documents!')
+        }
+
+        console.log(result.ops)
+    })
+})
+
