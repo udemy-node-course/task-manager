41caf3351e904aff07e4b93cbb0f95c61d644dfb
sgigli <simon.luciano@gigli.net>
2020-04-29 14:46:31 -0400

    Worked with create and read from mongodb driver

--- mongodb.js
+++ mongodb.js
@@ -1,7 +1,10 @@
 // CRUD create read update delete
 
-const mongodb = require('mongodb')
-const MongoClient = mongodb.MongoClient
+// const mongodb = require('mongodb')
+// const MongoClient = mongodb.MongoClient
+// const ObjectID = mongodb.ObjectID
+
+const { MongoClient, ObjectID } = require('mongodb')
 
 const connectionURL = 'mongodb://127.0.0.1:27017'
 const databaseName = 'task-manager'
@@ -13,18 +16,28 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) =>
 
     const db = client.db(databaseName)
 
-    // db.collection('users').insertOne({
-    //     name: 'Simon',
-    //     age: 25
-    // }, (error, result) => {
+    // db.collection('users').findOne({ _id: new ObjectID('5ea9a7759f290ed15086fb97')}, (error, user) => {
     //     if (error) {
-    //         return console.log('Unable to insert user')
+    //         return console.log('Unable to fetch')
     //     }
 
-    //     console.log(result.ops)
+    //     console.log(user)
     // })
 
-    // db.collection('users').insertMany([
+    // db.collection('users').find({ age: 25 }).toArray((error, users) => {
+    //     console.log(users)
+    // })
+
+    db.collection('tasks').findOne({ _id: new ObjectID('5ea8a85cffc8eec8861941d9')}, (error, user) => {
+        console.log(user)
+    })
+
+    db.collection('tasks').find({ completed: false }).toArray((error, tasks) => {
+        console.log(tasks)
+    })
+})
+
+// db.collection('users').insertMany([
     //     {
     //         name: 'Jen',
     //         age: 28
@@ -37,28 +50,3 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) =>
     //     if (error) {
     //         return console.log('Unable to insert documents!')
     //     }
-
-    //     console.log(result.ops)
-    // })
-
-    db.collection('tasks').insertMany([
-        {
-            description: 'dishes',
-            completed: true
-        }, {
-            description: 'homework',
-            completed: false
-        }, {
-            description: 'track practice',
-            completed: true
-        }
-
-    ], (error, result) => {
-        if (error) {
-            return console.log('Unable to insert documents!')
-        }
-
-        console.log(result.ops)
-    })
-})
-
