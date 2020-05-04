513eb3fedcf26c022d99ea3a403b30145431bcac
sgigli <simon.luciano@gigli.net>
2020-05-04 16:53:16 -0400

    Routes done; now w/ async/await; separate router files

--- playground/promise-chaining-2.js
+++ playground/promise-chaining-2.js
@@ -1,14 +1,29 @@
 require('../src/db/mongoose')
 const Task = require('../src/models/task')
 
-Task.findByIdAndDelete('5eaaf1009431adedf3410608')
-    .then((task) => {
-        console.log(task)
-        return Task.countDocuments({ completed: false })
-    })
+// Task.findByIdAndDelete('5eaaf1009431adedf3410608')
+//     .then((task) => {
+//         console.log(task)
+//         return Task.countDocuments({ completed: false })
+//     })
+//     .then((count) => {
+//         console.log(count)
+//     })
+//     .catch((e) => {
+//         console.log(error)
+//     })
+
+const deleteTaskAndCount = async (id) => {
+    const task = await Task.findByIdAndDelete(id)
+    console.log(task)
+    const count = Task.countDocuments({ completed: false })
+    return count
+}
+
+deleteTaskAndCount('5eaaff5435054df2979aac6e')
     .then((count) => {
         console.log(count)
     })
-    .catch((e) => {
+    .catch((error) => {
         console.log(error)
     })
