7a84ef2d47a03d8d2c8419ccadaac6e81bfa8c2f
sgigli <simon.luciano@gigli.net>
2020-05-06 17:32:41 -0400

    Auth for users/tasks complete

--- src/index.js
+++ src/index.js
@@ -26,14 +26,16 @@ app.listen(port, () => {
     console.log('Server is up on port ' + port)
 })
 
-const jwt = require('jsonwebtoken')
+const Task = require('./models/task')
+const User = require('./models/user')
 
-const myFunction = async () => {
-    const token = jwt.sign({ _id: 'abc123' }, 'thisismynewcourse', { expiresIn: '7 days' })
-    console.log(token)
-
-    const data = jwt.verify(token, 'thisismynewcourse')
-    console.log(data)
+const main = async () => {
+    // const task = await Task.findById('5eb31c898028a019a52676ee')
+    // await task.populate('owner').execPopulate()
+    // console.log(task.owner)
+    const user = await User.findById('5eb31c50f042e3196a19db90')
+    await user.populate('tasks').execPopulate()
+    console.log(user.tasks)
 }
 
-myFunction()
+main()
