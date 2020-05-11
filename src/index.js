6dbf41935963a6ab0a5e862fbb3c171ddf49c047
sgigli <simon.luciano@gigli.net>
2020-05-11 17:56:20 -0400

    File upload feature integrated

--- src/index.js
+++ src/index.js
@@ -6,18 +6,6 @@ const taskRouter = require('./routers/task')
 const app = express()
 const port = process.env.PORT || 3000
 
-// app.use((req, res, next) => {
-//     if (req.method === 'GET') {
-//         res.send('GET requests are disabled')
-//     } else {
-//         next()
-//     }
-// })
-
-// app.use((req, res, next) => {
-//     res.status(503).send('Sorry, temporarily under maintenance.')
-// })
-
 app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter)
@@ -25,22 +13,3 @@ app.use(taskRouter)
 app.listen(port, () => {
     console.log('Server is up on port ' + port)
 })
-
-const Task = require('./models/task')
-const User = require('./models/user')
-
-const main = async () => {
-    // const task = await Task.findById('5eb31c898028a019a52676ee')
-    // await task.populate('owner').execPopulate()
-    // console.log(task.owner)
-    try {
-        const user = await User.findById('5eb31c50f042e3196a19db90')
-        await user.populate('tasks').execPopulate()
-        console.log(user.tasks)
-    } catch (e) {
-        console.log('Something went wrong')
-    }
-    
-}
-
-main()
